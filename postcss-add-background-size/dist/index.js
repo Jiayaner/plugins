f30cdde2c15882e764548f1d4921b18e458320d4
冯嘉妍 <alice1@gm99.com>
2021-05-27 21:03:50 +0800

    提交

---
+++ postcss-add-background-size/dist/index.js
@@ -0,0 +1,48 @@
+const postcss = require("postcss");
+const path = require("path");
+const sizeOf = require("image-size");
+
+let fileInfoSession = new Map();
+
+module.exports = postcss.plugin("postcss-add-background-size", ({ cssPath = "" }) => {
+  return (root) => {
+    root.walkRules((rule) => {
+      rule.walkDecls(/^background-?/, ({ value }) => {
+        var resArr = value.match(/url\(("|')?([.\/a-z0-9A-Z_-]+)("|')?\)/);
+        // 网络图片不加载
+        if (resArr && resArr[2] && !/^http/.test(resArr[2])) {
+          let fileInfo = null;
+          if (fileInfoSession.has(resArr[2])) {
+            fileInfo = fileInfoSession.get(resArr[2]);
+          } else {
+            try {
+              var fileUrl = path.resolve(process.cwd(), cssPath, resArr[2]);
+              fileInfo = sizeOf(fileUrl);
+            } catch (error) {
+              console.log("图片读取错误");
+              throw error;
+            }
+          }
+          let { width, height } = fileInfo;
+          if (width && height) {
+            rule.append({
+              prop: "width",
+              value: `${width}px`,
+            });
+            rule.append({
+              prop: "height",
+              value: `${height}px`,
+            });
+            rule.append({
+              prop: "background-size",
+              value: "contain",
+            });
+            fileInfoSession.set(resArr[2], { width: width, height: height });
+          } else {
+            console.error(`设置图片${resArr[2]}大小失败`);
+          }
+        }
+      });
+    });
+  };
+});
