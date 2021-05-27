5ae0973d0a15a965934d41624fafd9148f2da48b
冯嘉妍 <alice1@gm99.com>
2021-05-27 21:08:17 +0800

    提交

--- px2vwh-loader/index.js
+++
@@ -1,8 +0,0 @@
-var loaderUtils = require('loader-utils');
-var Px2vwh = require('px2vwh');
-
-module.exports = function (source) {
-  var options = loaderUtils.getOptions(this);
-  var px2vwhInit = new Px2vwh(options);
-  return px2vwhInit.generateValue(source);
-};
